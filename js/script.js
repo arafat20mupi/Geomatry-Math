77aed0ede4bb23ce9e72533b5e62557076eafd13
Arafat Islam <arafat.innotimes.net@gmail.com>
2024-02-13 13:15:59 +0600

    Rectangle script added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Triangle 
+//--------------Triangle ---------------
 const triangleBase = document.getElementById('triangleBase')
 const triangleheight = document.getAnimations('triangleHeight')
 const triangleCalculator = document.getElementById('triangleCalculator')
@@ -10,4 +10,18 @@ triangleCalculator.addEventListener('click' ,function(){
     const parseHeight = parseFloat(heightNumber)
     const triangleArea = 0.5 * parseBase * parseHeight
     triangleAreaSpan.innerText  = triangleArea
+})
+
+// ---------rectangle--------------
+const rectangleCalculator = document.getElementById('rectangleCalculator');
+const rectangleWeight = document.getElementById('rectangleWeight');
+const rectangleLenght = document.getElementById('rectangleLenght');
+const rectangleAreaSpan =document.getElementById('rectangleAreaSpan');
+rectangleCalculator.addEventListener('click' , function(){
+    const weightNumber = rectangleWeight.value
+    const parseWeight = parseFloat(weightNumber)
+    const lenghtNumber = rectangleLenght.value
+    const parseLenght = parseFloat(lenghtNumber)
+    const rectangleArea = parseWeight * parseLenght
+    rectangleAreaSpan.innerText = rectangleArea
 })
